ea1e08a7ecd02a84592e9eed295860b7b2a56924
CarolinaPardoFuquen <c.pardo@uniandes.edu.co>
2018-08-18 16:47:47 -0500

    creacion de la base de datos

--- aucarvideo/aucarvideo/settings.py
+++ aucarvideo/aucarvideo/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'aucarvideo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'postgres', # Or path to database file if using sqlite3.
+    'USER': 'postgres', # Not used with sqlite3.
+    'PASSWORD': 'postgres', # Not used with sqlite3.
+    'HOST': '172.17.0.2', # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '5432', # Set to empty string for default. Not used with sqlite3.
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
